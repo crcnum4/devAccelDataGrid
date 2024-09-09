d9a63a91cf6ad1b607bee11daf9036a6ef33874a
Cliff Choiniere <crcnum4@gmail.com>
2024-09-09 06:39:23 -0400

    Data types not exporting

---
+++ example/src/Example.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FC } from 'react'
+// import { data } from './sampleData'
+import { ColumnOptions } from 'devaccel-data-grid'
+
+const Example: FC = () => {
+  const tableOptions: ColumnOptions[] = []
+
+  return <h1>test</h1>
+}
+
+export default Example
---
+++ example/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+  <React.StrictMode>
+    <div>
+      <h1>home</h1>
+    </div>
+  </React.StrictMode>,
+)
+
+export default root
